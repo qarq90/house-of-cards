032356e1aebfd8d9e81cffc4788ba9a20091a292
qarq90 <scythethroughtheheart05@gmail.com>
2026-01-15 01:25:55 +0530

    QOL - 15/01/2026

---
+++ card_probabilities.py
@@ -0,0 +1,148 @@
+griffon_cards = [
+    {"house": "griffon", "name": "0", "value": 0},
+    {"house": "griffon", "name": "0", "value": 0},
+    {"house": "griffon", "name": "1", "value": 1},
+    {"house": "griffon", "name": "1", "value": 1},
+    {"house": "griffon", "name": "2", "value": 2},
+    {"house": "griffon", "name": "2", "value": 2},
+    {"house": "griffon", "name": "3", "value": 3},
+    {"house": "griffon", "name": "3", "value": 3},
+    {"house": "griffon", "name": "4", "value": 4},
+    {"house": "griffon", "name": "4", "value": 4},
+    {"house": "griffon", "name": "5", "value": 5},
+    {"house": "griffon", "name": "5", "value": 5},
+    {"house": "griffon", "name": "6", "value": 6},
+    {"house": "griffon", "name": "6", "value": 6},
+    {"house": "griffon", "name": "7", "value": 7},
+    {"house": "griffon", "name": "7", "value": 7},
+    {"house": "griffon", "name": "8", "value": 8},
+    {"house": "griffon", "name": "8", "value": 8},
+    {"house": "griffon", "name": "9", "value": 9},
+    {"house": "griffon", "name": "9", "value": 9},
+    {"house": "griffon", "name": "mirror", "value": 0},
+    {"house": "griffon", "name": "hallows", "value": 0},
+    {"house": "griffon", "name": "echo", "value": 0},
+    {"house": "griffon", "name": "shield", "value": 0},
+    {"house": "griffon", "name": "swap", "value": 0},
+]
+
+hufflepuff_cards = [
+    {"house": "hufflepuff", "name": "0", "value": 0},
+    {"house": "hufflepuff", "name": "0", "value": 0},
+    {"house": "hufflepuff", "name": "1", "value": 1},
+    {"house": "hufflepuff", "name": "1", "value": 1},
+    {"house": "hufflepuff", "name": "2", "value": 2},
+    {"house": "hufflepuff", "name": "2", "value": 2},
+    {"house": "hufflepuff", "name": "3", "value": 3},
+    {"house": "hufflepuff", "name": "3", "value": 3},
+    {"house": "hufflepuff", "name": "4", "value": 4},
+    {"house": "hufflepuff", "name": "4", "value": 4},
+    {"house": "hufflepuff", "name": "5", "value": 5},
+    {"house": "hufflepuff", "name": "5", "value": 5},
+    {"house": "hufflepuff", "name": "6", "value": 6},
+    {"house": "hufflepuff", "name": "6", "value": 6},
+    {"house": "hufflepuff", "name": "7", "value": 7},
+    {"house": "hufflepuff", "name": "7", "value": 7},
+    {"house": "hufflepuff", "name": "8", "value": 8},
+    {"house": "hufflepuff", "name": "8", "value": 8},
+    {"house": "hufflepuff", "name": "9", "value": 9},
+    {"house": "hufflepuff", "name": "9", "value": 9},
+    {"house": "hufflepuff", "name": "mirror", "value": 0},
+    {"house": "hufflepuff", "name": "hallows", "value": 0},
+    {"house": "hufflepuff", "name": "echo", "value": 0},
+    {"house": "hufflepuff", "name": "shield", "value": 0},
+    {"house": "hufflepuff", "name": "swap", "value": 0},
+]
+
+ravenclaw_cards = [
+    {"house": "ravenclaw", "name": "0", "value": 0},
+    {"house": "ravenclaw", "name": "0", "value": 0},
+    {"house": "ravenclaw", "name": "1", "value": 1},
+    {"house": "ravenclaw", "name": "1", "value": 1},
+    {"house": "ravenclaw", "name": "2", "value": 2},
+    {"house": "ravenclaw", "name": "2", "value": 2},
+    {"house": "ravenclaw", "name": "3", "value": 3},
+    {"house": "ravenclaw", "name": "3", "value": 3},
+    {"house": "ravenclaw", "name": "4", "value": 4},
+    {"house": "ravenclaw", "name": "4", "value": 4},
+    {"house": "ravenclaw", "name": "5", "value": 5},
+    {"house": "ravenclaw", "name": "5", "value": 5},
+    {"house": "ravenclaw", "name": "6", "value": 6},
+    {"house": "ravenclaw", "name": "6", "value": 6},
+    {"house": "ravenclaw", "name": "7", "value": 7},
+    {"house": "ravenclaw", "name": "7", "value": 7},
+    {"house": "ravenclaw", "name": "8", "value": 8},
+    {"house": "ravenclaw", "name": "8", "value": 8},
+    {"house": "ravenclaw", "name": "9", "value": 9},
+    {"house": "ravenclaw", "name": "9", "value": 9},
+    {"house": "ravenclaw", "name": "mirror", "value": 0},
+    {"house": "ravenclaw", "name": "hallows", "value": 0},
+    {"house": "ravenclaw", "name": "echo", "value": 0},
+    {"house": "ravenclaw", "name": "shield", "value": 0},
+    {"house": "ravenclaw", "name": "swap", "value": 0},
+]
+
+slytherin_cards = [
+    {"house": "slytherin", "name": "0", "value": 0},
+    {"house": "slytherin", "name": "0", "value": 0},
+    {"house": "slytherin", "name": "1", "value": 1},
+    {"house": "slytherin", "name": "1", "value": 1},
+    {"house": "slytherin", "name": "2", "value": 2},
+    {"house": "slytherin", "name": "2", "value": 2},
+    {"house": "slytherin", "name": "3", "value": 3},
+    {"house": "slytherin", "name": "3", "value": 3},
+    {"house": "slytherin", "name": "4", "value": 4},
+    {"house": "slytherin", "name": "4", "value": 4},
+    {"house": "slytherin", "name": "5", "value": 5},
+    {"house": "slytherin", "name": "5", "value": 5},
+    {"house": "slytherin", "name": "6", "value": 6},
+    {"house": "slytherin", "name": "6", "value": 6},
+    {"house": "slytherin", "name": "7", "value": 7},
+    {"house": "slytherin", "name": "7", "value": 7},
+    {"house": "slytherin", "name": "8", "value": 8},
+    {"house": "slytherin", "name": "8", "value": 8},
+    {"house": "slytherin", "name": "9", "value": 9},
+    {"house": "slytherin", "name": "9", "value": 9},
+    {"house": "slytherin", "name": "mirror", "value": 0},
+    {"house": "slytherin", "name": "hallows", "value": 0},
+    {"house": "slytherin", "name": "echo", "value": 0},
+    {"house": "slytherin", "name": "shield", "value": 0},
+    {"house": "slytherin", "name": "swap", "value": 0},
+]
+
+def getShuffledDeck():
+    import random
+    completeDeck = griffon_cards + hufflepuff_cards + ravenclaw_cards + slytherin_cards
+    newDeck = []
+    for i in range(0, 11):
+        newDeck.append(completeDeck.pop(random.randint(0, len(completeDeck)-1)))
+    return newDeck
+       
+       
+player_A_cards = getShuffledDeck()
+player_B_cards = getShuffledDeck()
+player_C_cards = getShuffledDeck()
+player_D_cards = getShuffledDeck()
+
+print("Player A Cards:", player_A_cards, end='\n\n')
+print("Player B Cards:", player_B_cards, end='\n\n')    
+print("Player C Cards:", player_C_cards, end='\n\n')
+print("Player D Cards:", player_D_cards)
+
+def getHouseProbabilities(player_cards):
+    griffon_count = sum(1 for card in player_cards if card["house"] == "griffon")
+    hufflepuff_count = sum(1 for card in player_cards if card["house"] == "hufflepuff")
+    ravenclaw_count = sum(1 for card in player_cards if card["house"] == "ravenclaw")
+    slytherin_count = sum(1 for card in player_cards if card["house"] == "slytherin")
+    total_cards = len(player_cards)
+    return {
+        "griffon": griffon_count / total_cards,
+        "hufflepuff": hufflepuff_count / total_cards,
+        "ravenclaw": ravenclaw_count / total_cards,
+        "slytherin": slytherin_count / total_cards,
+    }
+    
+print("\nPlayer A House Probabilities:", getHouseProbabilities(player_A_cards))
+print("Player B House Probabilities:", getHouseProbabilities(player_B_cards))   
+print("Player C House Probabilities:", getHouseProbabilities(player_C_cards))
+print("Player D House Probabilities:", getHouseProbabilities(player_D_cards))
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="h-screen w-screen flex justify-center items-center flex-col gap-16">
-      <h1 className="playwrite-romania text-7xl">House of Cards</h1>
-    </main>
-  );
-}
